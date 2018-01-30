ddf001e73927fa8e614ae022c2bc00305fe48ba4
Nabil <nabiljesus@gmail.com>
2018-01-29 23:12:17 -0400

    freelancer project

---
+++ projects/data/freelancer/script.js
@@ -0,0 +1,34 @@
+text = '\
+<p> I\'ve been working as freelancer for about two years through online platforms like <i>Upwork, Freelancer and Fiverr</i>.</p>\
+<p> Doing so, I\'ve found interesting proporsals. For example, I\'ve made some animated SVG using only high order functions in Haskell. Likewise, I\'ve managed to webscrap multiple data from as search results from Google.</p>\
+<p> I have made applied some minor and major fixes on web applications running on Ruby on Rails and Django, as I\'ve developed new front-end interactions to improve the user experiences of those apps.</p>\
+<p> Sadly, I can\'t tell much about other jobs, but I\'m always trying to get new and interesting offers.</p>\
+'
+
+projects['freelancer'] = {
+	'title'  : 'Freelancer Jobs',
+	'desc'   : 'Some jobs as freelancer',
+	'text'   :  text,
+	'bullets': ['Haskell', 'Python', 'Ruby', "Javascript", "WebScrapping"],
+	'banner' : 'projects/data/freelancer/banner.png',
+	'cats'   : ['freelance'],
+	'images' : [
+	{
+		description: 'Haskell Animation Polygon',
+		href: 'projects/data/freelancer/images/1.svg',
+		poster: 'projects/data/freelancer/images/1.svg'
+	},
+	{
+		description: 'Haskell Animation Hyperspace',
+		href: 'projects/data/freelancer/images/2.svg',
+		poster: 'projects/data/freelancer/images/2.svg'
+	},
+	{
+		description: 'Haskell Animation, Bee',
+		href: 'projects/data/freelancer/images/3.svg',
+		poster: 'projects/data/freelancer/images/3.svg'
+	},
+	],
+	'videos' : [
+	],
+}
